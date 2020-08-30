929b3473a2fbefc6a7e171f1f16ea7fb441fc6ab
lightningbolts <timberlake2025@gmail.com>
2020-08-30 11:13:18 -0700

    Fun with absolutely stupidly big numbers including Ackermann's Function and Arrow Notation.

--- 1.2.1.js
+++ 1.2.1.js
@@ -38,5 +38,18 @@ function inc(c) {
 function dec(d) {
     return d - 1
 }
-console.log(plus_recursive(4, 5))
-console.log(plus_iterative(4, 5))
+//console.log(plus_recursive(4, 5))
+//console.log(plus_iterative(4, 5))
+
+function A(x, y) {
+    if (y === 0) {
+        return 0
+    } else if (x === 0) {
+        return 2 * y
+    } else if (y === 1) {
+        return 2
+    } else {
+        A((x - 1), A(x, y-1))
+    }
+}
+console.log(A(5, 2))
--- 1.2.1.rkt
+++ 1.2.1.rkt
@@ -20,3 +20,34 @@
       b
       (plusss (- a 1) (+ b 1))))
 (trace plusss)
+
+(define (A x y)
+  (if (= y 0)
+      0
+      (if (= x 0)
+          (* 2 y)
+          (if (= y 1)
+              2
+              (A (- x 1) (A x (- y 1)))))))
+(trace A)
+
+(define (h n)
+  (A 2 n))
+
+(trace h)
+
+(define (f n)
+  (A 1 n))
+
+(trace f)
+
+
+(define arrow expt)
+
+(define (arrow2 n)
+  (arrow n (arrow n n)))
+(trace arrow2)
+
+
+  
+
