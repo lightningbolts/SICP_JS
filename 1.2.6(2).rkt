72645f821e4d6caf53cb93681037168cccc38911
lightningbolts <timberlake2025@gmail.com>
2020-09-22 19:53:17 -0700

    Fermat Primality Test

---
+++ 1.2.6(2).rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (even n)
+  (= (remainder n 2)) 0)
+
+(define (square x)
+  (* x x))
+
+(define (expmod base expt m)
+  (if (= expt 0)
+      1
+      (if (even expt)
+          (remainder (square (expmod base (/ expt 2) m)) m)
+          (remainder (* base (expmod base (- expt 1) m)) m))))
+
+(define (fermat_test n)
+  (define (try_it a)
+    (= (expmod a n n) a))
+  (try_it (+ 1 (random (- n 1)))))
+
+(define (fast_is_prime n times)
+  (if (= times 0)
+      #t
+      (if (fermat_test n)
+          (fast_is_prime n (- times 1))
+          #f
+          )))
--- 1.2.6.js
+++ 1.2.6.js
@@ -1,3 +1,5 @@
+
+
 const { sqrt } = require("./1.1.8")
 
 function naive_prime(n) {
@@ -12,7 +14,7 @@ function naive_prime(n) {
     return true
 }
 
-console.log(naive_prime(37))
+//console.log(naive_prime(37))
 
 function smallest_divisor(n) {
     return find_divisor(n, 2)
@@ -41,5 +43,46 @@ function is_prime(n) {
     return n === smallest_divisor(n)
 }
 
-console.log(is_prime(9))
+//console.log(is_prime(9))
 
+function even(x) {
+    return x % 2 === 0
+}
+
+function square(x) {
+    return x ** 2
+}
+
+function expmod(base, expt, m) {
+    if (expt === 0) {
+        return 1
+    } else {
+        if (even(expt)) {
+            return square(expmod(base, expt / 2, m)) % m
+        } else {
+            return base * expmod(base, expt - 1, m) % m
+        }
+    }
+}
+
+function fermat_test(n) {
+    const a_1 = Math.floor(Math.random(n-1))
+    function try_it(a) {
+        return expmod(a, n, n) === a;
+    }
+    return try_it(1 + a_1)
+}
+
+function fast_is_prime(n, times) {
+    if (times === 0) {
+        return true
+    } else {
+        if (fermat_test(n)) {
+            return fast_is_prime(n, times - 1) 
+        } else {
+            return false
+        }
+    }
+}
+
+console.log(fast_is_prime(14879334839, 100000))
