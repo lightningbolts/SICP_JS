5f8c11dd6820baae71277d86916b4ea9eedcc138
lightningbolts <timberlake2025@gmail.com>
2020-10-05 19:47:50 -0700

    Calculus and Compounding

--- 1.3.1.js
+++ 1.3.1.js
@@ -30,7 +30,7 @@ function name(a, b, term, next) {
     if (a > b) {
         return 0
     } else {
-        return term(a) + name(next(a), b)
+        return term(a) + name(next(a), b, term, next)
     }
 }
 
@@ -49,7 +49,7 @@ function sum_cubes1(a, b) {
         return y + 1
     }
     return name(a, b, cube, next)
-}   
+}
 
 function pi_sum1(a, b) {
     function term(x) {
@@ -71,10 +71,36 @@ function integral(f, a, b, dx) {
 //console.log(cube, 0, 1, 0.001)
 
 function simpsonRule(f, a, b, n) {
+    const y_0 = f(a)
+    const h = (b - a) / n
+    const y_n = f(a + n * h)
     function term(a) {
-        return f(a + kh)
+        return f(a)
     }
-    function next(x) {
-        return x + h
+    function next(a) {
+        return a + 2 * h
     }
+    return (h / 3) * (y_0 + 4 * name(a + h, a + (n - 1) * h, term, next) + 2 * name(a + 2 * h, a + (n - 2) * h, term, next) + y_n)
 }
+
+function inc(k) {
+    return k + 1;
+}
+function simpsons_rule_integral(f, a, b, n) {
+    function helper(h) {
+        function y(k) { 
+            return f((k * h) + a);
+        }
+	function term(k) {
+            return k === 0 || k === n
+                   ? y(k)
+                   : k % 2 === 0
+                     ? 2 * y(k)
+                     : 4 * y(k);
+        }
+        return sum(term, 0, inc, n) * (h / 3);
+    }
+    return helper((b - a) / n);
+}
+
+console.log(simpsonRule(cube, 0, 1, 1000))
