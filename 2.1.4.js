811441707014fb313766bcbe6e25cd5d4014af1d
lightningbolts <timberlake2025@gmail.com>
2020-11-11 19:31:14 -0800

    Javascript problems...

--- 2.1.4.js
+++ 2.1.4.js
@@ -27,35 +27,35 @@ function the_trouble_maker(xl, xu, yl, yu) {
         math_max(p1, p2, p3, p4));
 }
 function mul_interval(x, y) {
-    const xl = lower_bound(x);
-    const xu = upper_bound(x);
-    const yl = lower_bound(y);
-    const yu = upper_bound(y);
+    const xl = lower_bound2(x);
+    const xu = upper_bound2(x);
+    const yl = lower_bound2(y);
+    const yu = upper_bound2(y);
     return p(xl) && p(xu) && p(yl) && p(yu)
-        ? make_interval(xl * yl, xu * yu)
+        ? make_interval2(xl * yl, xu * yu)
         : p(xl) && p(xu) && n(yl) && p(yu)
-            ? make_interval(xu * yl, xu * yu)
+            ? make_interval2(xu * yl, xu * yu)
             : p(xl) && p(xu) && n(yl) && n(yu)
-                ? make_interval(xu * yl, xl * yu)
+                ? make_interval2(xu * yl, xl * yu)
                 : n(xl) && p(xu) && p(yl) && p(yu)
-                    ? make_interval(xl * yu, xu * yu)
+                    ? make_interval2(xl * yu, xu * yu)
                     : n(xl) && p(xu) && n(yl) && n(yu)
-                        ? make_interval(xu * yl, xl * yl)
+                        ? make_interval2(xu * yl, xl * yl)
                         : n(xl) && n(xu) && p(yl) && p(yu)
-                            ? make_interval(xl * yu, xu * yl)
+                            ? make_interval2(xl * yu, xu * yl)
                             : n(xl) && n(xu) && n(yl) && p(yu)
-                                ? make_interval(xl * yu, xl * yl)
+                                ? make_interval2(xl * yu, xl * yl)
                                 : n(xl) && n(xu) && n(yl) && n(yu)
-                                    ? make_interval(xu * yu, xl * yl)
+                                    ? make_interval2(xu * yu, xl * yl)
                                     : n(xl) && p(xu) && n(yl) && p(yu)
                                         ? the_trouble_maker(xl, xu, yl, yu)
                                         : error("lower larger than upper");
 }
 
 function div_interval(x, y) {
-    show_interval(x)
-    show_interval(y)
-    console.log(width_interval(y))
+    //show_interval(x)
+    //show_interval(y)
+    //console.log(width_interval(y))
     if (width_interval(y) === 0) {
         Error("Cannot Comply...Span of interval y = 0...Restate Request");
     } else {
@@ -107,7 +107,7 @@ function width_interval(interval) {
     return (Math.abs(lower_bound2(interval) - upper_bound2(interval))) / 2
 }
 //Exercise 2.10
-show_interval(div_interval(make_interval2(9, 10), make_interval2(10, 10)))
+//show_interval(div_interval(make_interval2(9, 10), make_interval2(10, 10)))
 //show_interval(mul_interval(make_interval2(1, 2), make_interval2(2, 3)))
 //Exercise 2.12
 function make_center_width(c, w) {
@@ -124,5 +124,25 @@ function make_center_percent(c, p) {
     return make_interval2(c - c * p, c + c * p)
 }
 
-show_interval(make_center_percent(5, 0.4))
-show_interval(make_center_width(5, 2))
+//show_interval(make_center_percent(5, 0.4))
+//show_interval(make_center_width(5, 2))
+//Exercise 2.14
+function par1(r1, r2) {
+    return div_interval(mul_interval(r1, r2),
+        add_interval(r1, r2));
+}
+function par2(r1, r2) {
+    const one = make_interval2(1, 1);
+    return div_interval(one,
+        add_interval(div_interval(one, r1),
+            div_interval(one, r2)));
+}
+const a = make_center_percent(5, 0.01)
+const b = make_center_percent(10, 0.02)
+const one = make_interval2(1, 1)
+
+//show_interval(par2(a, a))
+//show_interval(par1(a, a))
+//show_interval(par1(a, b))
+//show_interval(par2(a, b))
+show_interval(mul_interval(div_interval(one, a), a))
